df1b3a501d29520ec8e308322d6876212b942b9d
Daniel Bor <danielebs175@gmail.com>
2024-01-22 15:31:34 -0600

    fix migrations order

---
+++ app/Filament/Resources/CuentaResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CuentaResource\Pages;
+use App\Filament\Resources\CuentaResource\RelationManagers;
+use App\Models\Cuenta;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CuentaResource extends Resource
+{
+    protected static ?string $model = Cuenta::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCuentas::route('/'),
+            'create' => Pages\CreateCuenta::route('/create'),
+            'edit' => Pages\EditCuenta::route('/{record}/edit'),
+        ];
+    }
+}
