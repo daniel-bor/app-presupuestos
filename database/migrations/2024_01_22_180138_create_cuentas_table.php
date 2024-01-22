c8db91032eaf09e11d2e70ebc515bb8999c0240f
Daniel Bor <danielebs175@gmail.com>
2024-01-22 14:13:19 -0600

    initial structure
    -Add migrations tables
    -Add models

---
+++ database/migrations/2024_01_22_180138_create_cuentas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cuentas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->text('descripcion')->nullable();
+            $table->string('codigo')->unique(); // Código único para la cuenta
+            $table->integer('estado')->default(1); // Puede ser utilizado para activar/desactivar una cuenta
+            $table->timestamps(); // Crea automáticamente las columnas 'created_at' y 'updated_at'
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cuentas');
+    }
+};
---
+++ database/migrations/2024_01_22_180340_create_cuenta_pais_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cuenta_pais', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('pais_id');
+            $table->unsignedBigInteger('cuenta_id');
+            $table->integer('estado')->default(1); // Puede ser utilizado para activar/desactivar la relación
+            $table->timestamps();
+
+            $table->foreign('pais_id')->references('id')->on('pais');
+            $table->foreign('cuenta_id')->references('id')->on('cuentas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cuenta_pais', function (Blueprint $table) {
+            $table->dropForeign(['pais_id']);
+            $table->dropForeign(['cuenta_id']);
+        });
+        Schema::dropIfExists('cuenta_pais');
+    }
+};
